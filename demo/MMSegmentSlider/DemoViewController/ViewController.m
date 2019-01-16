4221e47be20d1e531b488e4fd1c922f75f982ae0
Shem Alexis Chavez <schavez@dnamicro.com>
2019-01-15 17:16:05 -0800

    Update demo app

--- demo/MMSegmentSlider/DemoViewController/ViewController.m
+++ demo/MMSegmentSlider/DemoViewController/ViewController.m
@@ -13,13 +13,27 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
+    [self sampleWithImage];
+}
+
+- (void)sampleTextOnly {
     self.segmentSlider.values = @[@"$19", @"$99", @"$199", @"$299"];
     self.segmentSlider.labels = @[@"1 month", @"6 months", @"1 year", @"2 years"];
     self.segmentSlider.labelsFont = [UIFont fontWithName:@"HelveticaNeue-Light" size:14.0f];
     
     [self.segmentSlider addTarget:self action:@selector(sliderValueChanged) forControlEvents:UIControlEventValueChanged];
     [self updatePriceLabel];
+}
+
+- (void)sampleWithImage {
+    self.segmentSlider.values = @[@"$19", @"$99", @"$199", @"$299"];
+    self.segmentSlider.labels = @[@"1 month", @"6 months", @"1 year", @"2 years"];
+    self.segmentSlider.circlesRadius = 8.0;
+    self.segmentSlider.circlesRadiusForSelected = 8.0;
+    self.segmentSlider.selectedImageOffset = 8.0;
+    self.segmentSlider.selectedValueImage = [UIImage imageNamed:@"arrow_down_green"];
+    
+    [self.segmentSlider addTarget:self action:@selector(sliderValueChanged) forControlEvents:UIControlEventValueChanged];
 }
 
 - (void)sliderValueChanged
